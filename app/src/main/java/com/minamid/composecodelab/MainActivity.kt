1142b6ba3e51134ccbb96e8b92d3735686e9f2fc
Daniel <danielminami@hotmail.com>
2021-06-22 16:55:30 -0400

    Added LazyColumn and ListItems to the MainActivity

--- app/src/main/java/com/minamid/composecodelab/MainActivity.kt
+++ app/src/main/java/com/minamid/composecodelab/MainActivity.kt
@@ -3,36 +3,145 @@ package com.minamid.composecodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.minamid.composecodelab.ui.theme.ComposeCodeLabTheme
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeCodeLabTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+            MyApp{
+                MyScreenContent()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeCodeLabTheme {
-        Greeting("Android")
+fun MyApp(content: @Composable () -> Unit) {
+    ComposeCodeLabTheme() {
+        Surface(color = Color.Yellow) {
+            content()
+        }
     }
 }
+
+@Composable
+fun Greeting(name: String) {
+    var isSelected by remember { mutableStateOf(false) }
+    val backgroundColor by animateColorAsState(if (isSelected) Color.Red else Color.Transparent)
+
+    Text(
+        text = "Hello $name!",
+        modifier = Modifier
+            .padding(24.dp)
+            .background(color = backgroundColor)
+            .clickable(onClick = { isSelected = !isSelected })
+    )
+}
+
+@Preview
+@Composable
+fun DefaultPreview() {
+    MyApp{
+        MyScreenContent()
+    }
+}
+
+@Composable
+fun MyScreenContent(names: List<String> = List(1000) { "Hello Android #$it" }) {
+    val counterState = remember { mutableStateOf(0) }
+
+    Column(modifier = Modifier.fillMaxHeight()) {
+        NameList(names,
+            Modifier
+                .weight(1f)
+                .padding(24.dp))
+        Counter(
+            count = counterState.value,
+            updateCount = { newCount ->
+                counterState.value = newCount
+            }
+        )
+    }
+}
+
+@Composable
+fun Counter(count: Int, updateCount: (Int) -> Unit) {
+    Button(
+        onClick = { updateCount(count+1) },
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = if (count > 5) Color.Green else Color.White
+        )
+    ) {
+        Text("I've been clicked $count times")
+    }
+}
+
+@Composable
+fun NameList(names: List<String>, modifier: Modifier = Modifier) {
+    LazyColumn(modifier = modifier) {
+        items(items = names) { name ->
+            Greeting(name = name)
+            Divider(color = Color.Black)
+        }
+    }
+}
+
+//@Composable
+//fun NewsStory() {
+//    MaterialTheme {
+//        val typography = MaterialTheme.typography
+//        Column(
+//            modifier = Modifier.padding(16.dp)
+//        ) {
+//            Image(
+//                painter = painterResource(R.drawable.ic_launcher_background),
+//                contentDescription = null,
+//                modifier = Modifier
+//                    .height(180.dp)
+//                    .fillMaxWidth()
+//                    .clip(shape = RoundedCornerShape(4.dp)),
+//                contentScale = ContentScale.Crop
+//            )
+//            Spacer(Modifier.height(16.dp))
+//
+//            Text(
+//                "A day wandering through the sandhills " +
+//                        "in Shark Fin Cove, and a few of the " +
+//                        "sights I saw",
+//                style = typography.h6,
+//                maxLines = 2,
+//                overflow = TextOverflow.Ellipsis)
+//            Text("Davenport, California",
+//                style = typography.body2)
+//            Text("December 2018",
+//                style = typography.body2)
+//        }
+//    }
+//}
+
